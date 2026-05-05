667e0d36636721dbb79de867b877e8a7348dcccb
smz785 <20480538@fhda.edu>
2026-05-05 01:23:37 -0700

    updated Course.java with skeleton code

--- src/main/java/org/example/cs151courseregapp/model/Course.java
+++ src/main/java/org/example/cs151courseregapp/model/Course.java
@@ -1,4 +1,31 @@
 package org.example.cs151courseregapp.model;
 
 public class Course {
+    private String courseCode;
+    private String title;
+    private int credits;
+    private String description;
+
+    public Course(String courseCode, String title, int credits, String description) {
+        this.courseCode = courseCode;
+        this.title = title;
+        this.credits = credits;
+        this.description = description;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
