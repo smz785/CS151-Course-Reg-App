6723e3bb82b11d82bda921446a3a4b873b7d801f
Asmita Dulla <79378258+asmitadulla@users.noreply.github.com>
2026-05-07 18:03:39 -0700

    Update AdminController.java

--- src/main/java/org/example/cs151courseregapp/controller/AdminController.java
+++ src/main/java/org/example/cs151courseregapp/controller/AdminController.java
@@ -1,4 +1,41 @@
 package org.example.cs151courseregapp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class AdminController {
+
+    @FXML
+    private TextField courseNameField;
+
+    @FXML
+    private ListView<String> courseList;
+
+    @FXML
+    public void initialize() {
+        // sample data
+        courseList.getItems().add("CS 46B");
+        courseList.getItems().add("CS 151");
+        courseList.getItems().add("MATH 42");
+    }
+
+    @FXML
+    private void addCourse() {
+        String courseName = courseNameField.getText();
+
+        if (courseName != null && !courseName.trim().isEmpty()) {
+            courseList.getItems().add(courseName.trim());
+            courseNameField.clear();
+        }
+    }
+
+    @FXML
+    private void removeCourse() {
+        String selectedCourse = courseList.getSelectionModel().getSelectedItem();
+
+        if (selectedCourse != null) {
+            courseList.getItems().remove(selectedCourse);
+        }
+    }
 }
