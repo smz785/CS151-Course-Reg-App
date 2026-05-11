9b5d709d7486ed354d8491fe9a08f2464a76b2e5
geeyana <gpnicomedes@gmail.com>
2026-05-10 17:00:42 -0700

    back button

--- src/main/java/org/example/cs151courseregapp/controller/ProfessorController.java
+++ src/main/java/org/example/cs151courseregapp/controller/ProfessorController.java
@@ -128,4 +128,12 @@ public class ProfessorController {
             }
         }
     }
+
+    @FXML
+    private void goBackToMain() throws java.io.IOException {
+        javafx.fxml.FXMLLoader fxmlLoader = new javafx.fxml.FXMLLoader(getClass().getResource("/org/example/cs151courseregapp/view/main-view.fxml"));
+        javafx.scene.Scene scene = new javafx.scene.Scene(fxmlLoader.load(), 600, 400);
+        javafx.stage.Stage stage = (javafx.stage.Stage) classesListView.getScene().getWindow();
+        stage.setScene(scene);
+    }
 }
